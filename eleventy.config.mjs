bac40d05d6ba0c6eea99f4ab9b5d94f20159c708
autowattage <pipandw@gmail.com>
2026-01-27 23:41:57 -0600

    customizable calendar includes, date formatting, mobile blog post tweaks

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,27 +1,18 @@
 import { DateTime } from "luxon";
-const TIME_ZONE = "UTC-6";
 
 export default async function(eleventyConfig) {
+	// copy /public folder to _site
   eleventyConfig.addPassthroughCopy({"public": "public"});
 
+  // enable inserting HTML in markdown
   eleventyConfig.setNunjucksEnvironmentOptions({
 		throwOnUndefined: true,
-		autoescape: false, // warning: don’t do this! <- NEVER!
+		autoescape: false, // warning: don’t do this! <- NEVER! <- No.
 	});
-
-	eleventyConfig.addDateParsing(function(dateValue) {
-		let localDate;
-		if(dateValue instanceof Date) { // and YAML
-			localDate = DateTime.fromJSDate(dateValue, { zone: "utc" }).setZone(TIME_ZONE, { keepLocalTime: true });
-		} else if(typeof dateValue === "string") {
-			localDate = DateTime.fromISO(dateValue, { zone: TIME_ZONE });
-		}
-		if (localDate?.isValid === false) {
-			throw new Error(`Invalid \`date\` value (${dateValue}) is invalid for ${this.page.inputPath}: ${localDate.invalidReason}`);
-		}
-		return localDate;
+	// date formatting for blog posts
+	eleventyConfig.addFilter("postDate", (dateObj, format, zone) => {
+		return DateTime.fromJSDate(dateObj, { zone: zone || "cst" }).toFormat(format || "DDD, T ZZZZ");
 	});
-	
 };
 
 export const config = {
